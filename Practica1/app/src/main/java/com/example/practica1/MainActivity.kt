dbe7cab4ed440759fc3e957f9708865b372b8690
unknown <143969875+FerGuHe@users.noreply.github.com>
2023-09-10 19:47:48 -0600

    Actividad 1

---
+++ Practica1/app/src/main/java/com/example/practica1/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.practica1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+//Actividad 1
+fun main(){
+    //print("Hello UPIICSA ")
+    //println("GUMDAM")
+
+    println("Hello UPIICSA ")
+    println("GUMDAM")
+}
+
