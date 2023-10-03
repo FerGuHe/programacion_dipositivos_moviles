e080b9677e0f505d8542109b1da269c5a512748d
unknown <143969875+FerGuHe@users.noreply.github.com>
2023-10-02 21:34:55 -0600

    Actividades_Clase

---
+++ Actividades_Clase/String temples_Operadores.kt
@@ -0,0 +1,34 @@
+package com.example.actividadesenclase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.net.PasswordAuthentication
+import kotlin.math.roundToInt
+import java.util.Scanner
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/**
+Suma y string templates.
+ */
+
+const val number2 = 0
+
+fun main() {
+
+    val edad = 23
+    println("Mi edad es $edad")
+    println("Mi edad dentro de 2 años sera: ${edad+2} años")
+
+}
+
+
+
+
+
