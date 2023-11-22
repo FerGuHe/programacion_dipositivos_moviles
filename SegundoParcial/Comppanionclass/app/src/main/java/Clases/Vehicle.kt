0c35cad643ef9e31be3101aede82577c2ae94f9a
unknown <143969875+FerGuHe@users.noreply.github.com>
2023-11-22 16:49:23 -0600

    SegundoParcial

---
+++ SegundoParcial/Comppanionclass/app/src/main/java/Clases/Vehicle.kt
@@ -0,0 +1,11 @@
+package Clases
+
+class Vehicle {
+    init {
+        println("vehiculo creado")
+    }
+    companion object Factory{
+        fun create (): Vehicle = Vehicle()
+    }
+    val vehicleIntance = Vehicle.create()
+}
